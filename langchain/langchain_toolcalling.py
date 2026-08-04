4e5741f589f75993ed552ab3934010518d740204
Ramakrishnan <ramakrishnanra88@gmail.com>
2026-08-04 06:39:49 +0530

    runnablePassthrough added

--- langchain/langchain_toolcalling.py
+++ langchain/langchain_toolcalling.py
@@ -5,13 +5,13 @@ os.environ["NVIDIA_API_KEY"] = "nvapi-ewmamM_EAk2NfDYQnSW8BYhWx2aOCfvwH1LQY3eHrG
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_nvidia_ai_endpoints import ChatNVIDIA
 from langchain_core.output_parsers import StrOutputParser
-
+from langchain_core.runnables import RunnablePassthrough
 # Chain setup with updated model ID
 prompt = ChatPromptTemplate.from_template("Tell me a short joke about {topic}")
-
+add_style = RunnablePassthrough.assign(style=lambda x: "dad joke")
 model = ChatNVIDIA(model="deepseek-ai/deepseek-v4-pro")
 output_parser = StrOutputParser()
-chain = prompt | model | output_parser
+chain = add_style | prompt | model | output_parser
 
-result = chain.invoke({"topic": "chickens"})
+result = chain.invoke({"topic": "cows"})
 print(result)
